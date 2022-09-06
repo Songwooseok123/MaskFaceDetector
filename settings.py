95dad7bd3fe820668a73485303a0ef0d034ee065
Songwooseok123 <80091008+Songwooseok123@users.noreply.github.com>
2022-09-06 12:55:09 +0900

    settings

---
+++ settings.py
@@ -0,0 +1,9 @@
+DATA_PATH = "/datasets/Face Mask Dataset"
+# DATA_PATH = "../data/catsanddogs"
+PS C:\Users\Songwooseok\BDML_STUDY2022\MINIproject
+IMAGE_SIZE = 64
+
+FACE_MASK_DETECTOR_CKPT = "./ckpts/face_mask_detector.pt"
+FACE_DETECTOR_CKPT = "./SFD_pytorch/data/s3fd_convert.pth"
+
+CUDA = True
